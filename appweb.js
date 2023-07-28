7cf4bc37d3775dcc126ecc0e60c209b950b81fa6
Ezequielprograma <ezequiel.medina.profesional@gmail.com>
2023-07-28 17:44:12 -0300

    Agregación de consultas avanzadas

--- appweb.js
+++ appweb.js
@@ -24,14 +24,110 @@ aplicacion.get('/',function(peticion,respuesta){
             respuesta.render('index',{productos:filas})
         })
        
-
         connection.release()
     })
 })
 
+aplicacion.get('/actualizar-formulario', function(peticion,respuesta){
 
+    pool.getConnection(function(err,connection){
 
+        const query = `SELECT * FROM inventario.productos WHERE id = ${connection.escape(peticion.query.id)}`
+
+        connection.query(query,function(error,filas,campos){
+            respuesta.render('actualizar',{producto:filas[0]})
+        })
+        connection.release()
+
+    }
+
+    )
+
+})
+
+aplicacion.post('/actualizar-tarea',function(peticion,respuesta){
+
+    pool.getConnection(function(err,connection){
+
+        const query = `UPDATE inventario.productos SET nombre=${connection.escape(peticion.body.nombre)},
+                                                    precio=${connection.escape(peticion.body.precio)},
+                                                    cantidad=${connection.escape(peticion.body.cantidad)}
+                                                    WHERE id=${connection.escape(peticion.body.id)} `
+
+        connection.query(query,function(error,filas,campos){
+            respuesta.redirect("/")
+        })
+
+        connection.release()
+    })
+
+})
+
+aplicacion.get('/formulario-Xprecio',function(peticion,respuesta){
+
+    pool.getConnection(function(err,connection){
+        const query = `SELECT * FROM inventario.productos ORDER BY productos.precio;`
+
+        connection.query(query,function(error,filas,campos){
+                respuesta.render('index',{productos:filas})
+        })
+
+        connection.release()
+    })
+
+}
+)
+
+aplicacion.get('/formulario-Xnombre',function(peticion,respuesta){
+
+    pool.getConnection(function(err,connection){
+        const query = `SELECT * FROM inventario.productos ORDER BY productos.nombre ASC;`
+
+        connection.query(query,function(error,filas,campos){
+                respuesta.render('index',{productos:filas})
+        })
+
+        connection.release()
+    })
+
+}
+
+)
+
+aplicacion.get('/formulario-Xcantidad',function(peticion,respuesta){
+
+    pool.getConnection(function(err,connection){
+        const query = `SELECT * FROM inventario.productos ORDER BY productos.cantidad ASC;`
+
+        connection.query(query,function(error,filas,campos){
+                respuesta.render('index',{productos:filas})
+        })
+
+        connection.release()
+    })
+
+}
+
+)
+
+aplicacion.get('/formulario-Xid',function(peticion,respuesta){
+
+    pool.getConnection(function(err,connection){
+        const query = `SELECT * FROM inventario.productos ORDER BY productos.id ASC;`
+
+        connection.query(query,function(error,filas,campos){
+                respuesta.render('index',{productos:filas})
+        })
+
+        connection.release()
+    })
+
+}
+
+)
 
 aplicacion.listen(8080,function(){
+
     console.log("Servidor Iniciado")
+
 })
